2fdcbc9d751204311d70c0c0fb4a05506e2b6074
Bhushan Patel <patelba@college.harvard.edu>
2021-09-24 12:45:50 -0400

    final draft (all test passing)

--- CS61/pset1/m61.cc
+++ CS61/pset1/m61.cc
@@ -25,10 +25,10 @@ long* heap_list_min;
 long* heap_list_max = 0;
 long file_number;
 
-std::map<void*, long> mp;
-std::unordered_map<void*, long> line_number;
-std::map<std::pair<const char*, long>, long> line_size;
-std::unordered_set<void*> doublefree_checker;
+std::unordered_map<void*, long> mp; // map to store active malloc calls
+std::unordered_map<void*, long> line_number; // map to store active malloc line numbers
+std::map<std::pair<const char*, long>, long> file_line_storage; // map to store heavy hitters info
+std::unordered_set<void*> doublefree_checker; // map to check freed pointers
 
 /// m61_malloc(sz, file, line)
 ///    Return a pointer to `sz` bytes of newly-allocated dynamic memory.
@@ -39,32 +39,35 @@ std::unordered_set<void*> doublefree_checker;
 void* m61_malloc(size_t sz, const char* file, long line) {
     (void) file, (void) line;   // avoid uninitialized variable warnings
     file_number = (long) *file;
-    if ((sz*100/sz) != 100) {
+    if ((sz*100/sz) != 100) { // check for integer overflow
       ++nfail_var;
       fail_size_var += sz;
       return nullptr;
     }
     char* b_m = (char*)base_malloc(sz + 1); //Allocate extra spot for metadata
     if (b_m) {
-      b_m[sz] = (long int) 50; //Assign canary value
+      b_m[sz] = (long int) 50; // Assign canary value
     }
-    long int* b_m_1 = (long int*) b_m; //Cast to long int to work with heap variables
+    long int* b_m_1 = (long int*) b_m; // Cast to long int to work with
+    //heap variables
 
     // Your code here.
     // if m61_malloc returns a NULL pointer, the allocation failed
     if (b_m_1 != NULL) {
       // Increase ntotal_var everytime malloc is called
       ++ntotal_var;
-      // Increase total_size_var by the size of argument, which is number of bytes allocated
+      // Increase total_size_var by the size of argument, which is
+      // number of bytes allocated
       total_size_var += sz;
-      // Use unordered map to keep track of active bytes corresponding to a malloc-returned pointer
+      // Use unordered map to keep track of active bytes corresponding to a
+      // malloc-returned pointer
       active_size_var += sz;
       mp[b_m_1] = sz;
-      if (line_size.count({file, line}) != 0) {
-        line_size[{file, line}] += sz;
+      if (file_line_storage.count({file, line}) != 0) {
+        file_line_storage[{file, line}] += sz;
       }
       else {
-        line_size[{file, line}] = sz;
+        file_line_storage[{file, line}] = sz;
       }
       line_number[b_m_1] = line;
       //Initialize min and max heap if NULL
@@ -85,7 +88,7 @@ void* m61_malloc(size_t sz, const char* file, long line) {
       }
 
     }
-    else {
+    else { // Icrement fail variable if returned pointer is null
       ++nfail_var;
       fail_size_var += sz;
     }
@@ -102,35 +105,44 @@ void* m61_malloc(size_t sz, const char* file, long line) {
 void m61_free(void* ptr, const char* file, long line) {
     (void) file, (void) line;   // avoid uninitialized variable warnings
     // Your code here.
-    if (ptr != nullptr && mp[ptr]) { //Second argument ensure input pointer is returned from malloc
-      long actual_size = (long) (mp[ptr] + 1); // Get size of allocated block from map
-      char* ptr_1 = (char*) ptr;  // Cast input ptr to char type
-      if (ptr_1[actual_size - 1] != 50) { // Checks if the "metadata" has been changed
+    // Ensures input pointer is returned from malloc
+    if (ptr != nullptr && mp[ptr]) {
+      long actual_size = (long) (mp[ptr] + 1); // Get size of allocated block
+      // from map and add  1 for metadata
+      char* ptr_1 = (char*) ptr;  // Cast input ptr to char type to index
+      // Checks if the "metadata" has been changed
+      if (ptr_1[actual_size - 1] != 50) {
         fprintf(stderr, "MEMORY BUG: detected wild write during free of pointer %p\n", ptr);
         abort();
       }
       ++free_var;
-      free_var_bytes += (long) mp[ptr]; //Get number of active allocated bytes from map
+      //Get number of active allocated bytes from map
+      free_var_bytes += (long) mp[ptr];
       doublefree_checker.insert(ptr);
-      mp.erase(ptr); //Remove pointer from map after freeing to keep metadata bounded
+      //Remove pointer from map after freeing to keep metadata bounded
+      mp.erase(ptr);
       base_free(ptr);
     }
-    else if (ptr != nullptr && doublefree_checker.count(ptr) != 0){//Second argument checks
-      //if ptr has been freed already, output error
+    //Second argument checks if ptr has been freed already
+    else if (ptr != nullptr && doublefree_checker.count(ptr) != 0){
       fprintf(stderr, "MEMORY BUG: invalid free of pointer %p, double free\n", ptr);
       abort();
     }
     else if (ptr == nullptr) {
       base_free(ptr);
     }
+    // check if not in heap
     else if (ptr < heap_list_min || ptr > heap_list_max) {
       fprintf(stderr, "MEMORY BUG: test%s.cc:%li: invalid free of pointer %p, not in heap\n", file, line, ptr);
       abort();
     }
+    // check for "not allocated" errors
     else if (!mp[ptr]) {
       fprintf(stderr, "MEMORY BUG: test%s.cc:%li: invalid free of pointer %p, not allocated\n", file, line, ptr);
       for (auto it = mp.begin(); it != mp.end(); ++it) {
+        // check if pointer is between the start of memory block and end
         if (ptr > it->first && (uintptr_t)ptr < ((uintptr_t)it->first + (uintptr_t)it->second)) {
+          // if it is, find difference and print out error
           long difference = (uintptr_t) ptr - (uintptr_t) it->first;
           fprintf(stderr, "test%li.cc:9: %p is %li bytes inside a %li byte region allocated here", file_number, ptr, difference, it->second);
           abort();
@@ -150,7 +162,7 @@ void m61_free(void* ptr, const char* file, long line) {
 
 void* m61_calloc(size_t nmemb, size_t sz, const char* file, long line) {
     // Your code here (to fix test019).
-    if ((nmemb * sz)/nmemb != sz){
+    if ((nmemb * sz)/nmemb != sz){ // overflow check
       nfail_var++;
       return nullptr;
     }
@@ -206,6 +218,7 @@ void m61_print_statistics() {
 
 void m61_print_leak_report() {
     // Your code here.
+    // Iterates through map of active allocations that are not freed
     for (auto it = mp.begin(); it != mp.end(); ++it) {
       fprintf(stdout, "LEAK CHECK: test%li.cc:%li: allocated object %p with size %li\n", file_number, line_number[it->first], it->first, it->second);
     }
@@ -217,17 +230,22 @@ void m61_print_leak_report() {
 
 void m61_print_heavy_hitter_report() {
     // Your heavy-hitters code here
+    // create map to store and sort >20% byte values
     std::map<long, std::pair<const char*, long>> sort_map;
     long total_map_bytes = 0;
-    for (auto it = line_size.begin(); it != line_size.end(); ++it) {
+    // iterate through all values to sum them
+    for (auto it = file_line_storage.begin(); it != file_line_storage.end(); ++it) {
       total_map_bytes += it->second;
     }
-    for (auto it = line_size.begin(); it != line_size.end(); ++it) {
+    // Iterate through map and divide each value by total bytes to get percent
+    for (auto it = file_line_storage.begin(); it != file_line_storage.end(); ++it) {
       float byte_fraction = ((float) it->second / (float) total_map_bytes);
+      // if greater then 20%, add to sorted map
       if (byte_fraction >= 0.20) {
         sort_map[it->second] = {it->first.first, it->first.second};
       }
     }
+    // iterate through ordered map in reverse
     for (auto it = sort_map.rbegin(); it != sort_map.rend(); ++it) {
       float byte_percent = 100*((float) it->first / (float) total_map_bytes);
       fprintf(stdout, "HEAVY HITTER: %s:%li: %li bytes (~%.1f%%)\n", it->second.first, it->second.second, it->first, byte_percent);
